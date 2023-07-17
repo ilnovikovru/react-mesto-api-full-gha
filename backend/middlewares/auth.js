488086f2a3d711dd84fefb41aa8838d5275b735f
Ilya Novikov <ilnovikov@ilnovikov.ru>
2023-07-17 17:17:26 +0100

    version 1.25

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,20 +1,23 @@
-const { NODE_ENV, JWT_SECRET, DEV_SECRET } = process.env;
+const { NODE_ENV, JWT_SECRET } = process.env;
 const jwt = require('jsonwebtoken');
 const UnauthorizedError = require('../errors/UnauthorizedError');
 
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
+  if (!authorization || !authorization.startsWith('Bearer ')) {
+    throw new UnauthorizedError('Необходима авторизация');
+  }
 
-  if (authorization) {
-    let payload;
-    const token = authorization.replace('Bearer ', '');
-    try {
-      payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : DEV_SECRET);
-    } catch (err) {
-      return next(new UnauthorizedError('Необходима авторизация'));
-    }
+  const token = authorization.split(' ')[1];
 
-    req.user = payload;
-    return next();
-  } return next(new UnauthorizedError('Необходима авторизация'));
+  let payload;
+  try {
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
+  } catch (err) {
+    throw new UnauthorizedError('Необходима авторизация');
+  }
+
+  req.user = payload;
+
+  return next();
 };
