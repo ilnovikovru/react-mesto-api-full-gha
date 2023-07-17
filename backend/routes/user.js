5bdfa52cd1ee31032f00f633f8c8b4a0e0f85a34
Ilya Novikov <ilnovikov@ilnovikov.ru>
2023-07-17 14:20:52 +0100

    version 1.22

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -6,10 +6,10 @@ const { validateObjId, validateUserUpdate, validateAvatarUpdate } = require('../
 
 const router = express.Router();
 
-router.get('/api//users', getUsers);
-router.get('/api//users/me', getUserInfo);
-router.patch('/api//users/me', validateUserUpdate, updateUserInfo);
-router.patch('/api//users/me/avatar', validateAvatarUpdate, updateAvatar);
-router.get('/api//users/:id', validateObjId, getUserById);
+router.get('/api/users', getUsers);
+router.get('/api/users/me', getUserInfo);
+router.patch('/api/users/me', validateUserUpdate, updateUserInfo);
+router.patch('/api/users/me/avatar', validateAvatarUpdate, updateAvatar);
+router.get('/api/users/:id', validateObjId, getUserById);
 
 module.exports = router;
