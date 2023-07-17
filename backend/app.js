3d621bf34967fd23b59339c4b9d622517e2716f9
Ilya Novikov <ilnovikov@ilnovikov.ru>
2023-07-17 09:34:07 +0100

    version 1.17

--- backend/app.js
+++ backend/app.js
@@ -20,6 +20,28 @@ app.use(express.json());
 
 app.use(requestLogger);
 
+const allowedCors = [
+  'https://ilnovikovru.nomoredomains.work',
+];
+
+app.use((req, res, next) => {
+  const { origin, method } = req;
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  const requestHeaders = req.headers['access-control-request-headers'];
+
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', '*');
+  }
+
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    return res.status(200).end();
+  }
+
+  return next();
+});
+
 app.get('/crash-test', () => {
   setTimeout(() => {
     throw new Error('Сервер сейчас упадёт');
